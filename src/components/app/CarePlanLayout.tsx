1a484f94df9c38fcf5f0eee17b29602838f20f25
navvailabs <navv.ai.labs@gmail.com>
2025-10-28 20:05:01 +0800

    Commit from Dualite Alpha:

--- src/components/app/CarePlanLayout.tsx
+++ src/components/app/CarePlanLayout.tsx
@@ -36,10 +36,10 @@ const CarePlanLayout = ({ children, activeView, setActiveView }: CarePlanLayoutP
                         <AnimatePresence mode="wait">
                             <motion.div
                                 key={activeView}
-                                initial={{ opacity: 0, y: 20 }}
-                                animate={{ opacity: 1, y: 0 }}
-                                exit={{ opacity: 0, y: -20 }}
-                                transition={{ duration: 0.3 }}
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 0.25, ease: 'easeInOut' }}
                             >
                                 {children}
                             </motion.div>
